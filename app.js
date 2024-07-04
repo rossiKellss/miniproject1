980e11e4afad50e95b0f66ca1e9b31f2fd1919f1
Rossi-Kellss <rossi.codingspace@gmail.com>
2024-07-04 21:58:03 +0545

    part 1 completed

--- app.js
+++ app.js
@@ -14,8 +14,28 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const isLoggedIn = (req, res, next) => {
+  const cookie = req.cookies.token;
+  if (!cookie) return res.send("you must be logged in");
+  else {
+    const data = verifyToken(cookie);
+    req.user = data;
+    next();
+  }
+};
+
 app.get("/", (req, res) => {
   res.render("index");
+});
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.get("/profile", isLoggedIn, (req, res) => {
+  console.log(req.user);
+  res.send("profile page");
 });
 
 app.post("/register", async (req, res, next) => {
@@ -34,11 +54,12 @@ app.post("/register", async (req, res, next) => {
     res.status(404).send("Error generating token");
   }
 
-  res.send(createUser);
+  res.cookie("token", token).redirect("/");
 });
 
 app.post("/login", async (req, res) => {
   const { email, password } = req.body;
+  console.log("received pass is", password);
   const emailExists = await User.findOne({ email });
 
   if (!emailExists) {
@@ -47,12 +68,16 @@ app.post("/login", async (req, res) => {
 
   const verifiedPassword = await bcrypt.compare(password, emailExists.password);
   if (!verifiedPassword) {
-    res.status(404).send("Invalid credentials");
-  } else {
-    const verifiedToken = signToken(emailExists._id);
-    res.cookie("token", verifiedToken).send("login successful");
+    res.status(404).render("login");
   }
+  const token = signToken(emailExists._id);
+  res.cookie("token", token).redirect("/");
 });
+app.get("/logout", (req, res) => {
+  res.cookie("token", "").redirect("/login");
+});
+
+
 
 app.listen(3000, (req, res) => {
   console.log("server running");
